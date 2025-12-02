83da0431b8354213bdcc6290497a1c46cb4b1da9
Denise Franz <denisealiciafranz@gmail.com>
2025-12-02 15:09:40 +0100

    code-update presentation

--- STEM3LieDetector/lieDetector.py
+++ STEM3LieDetector/lieDetector.py
@@ -3,7 +3,6 @@ import argparse
 import serial
 from serial.tools import list_ports   
 import sys
-from touch_capture import BAUD_RATE
 import mediapipe as mp
 import cv2
 import numpy as np
@@ -32,6 +31,7 @@ MPImage = mp.Image
 def main(port):
     print("Main")
     global rounds
+    current_round = rounds
 
     # --- RPS Mapping ---
     GESTURE_TO_MOVE = {
@@ -49,8 +49,11 @@ def main(port):
     pred_buffer = []      # last N predictions for smoothing
     BUFFER_SIZE = 15
     current_move = None   # last smoothed move
+    computer_text = ""
 
-    while rounds > 0:
+    
+    ser = serial.Serial(port, 115200, timeout = 1.0)
+    while current_round > 0:
         ret, frame = cap.read()
         if not ret:
             break
@@ -103,9 +106,10 @@ def main(port):
                     cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 2)
         cv2.putText(frame, move_text, (10, 65),
                     cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 0), 2)
-        cv2.putText(frame, "SPACE: play  |  Q: quit", (10, frame.shape[0] - 20),
+        cv2.putText(frame, "START: play  |  Q: quit", (10, frame.shape[0] - 20),
                     cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 2)
-
+        
+        
         cv2.imshow("RPS with gesture_recognizer.task", frame)
 
         key = cv2.waitKey(30) & 0xFF
@@ -115,35 +119,29 @@ def main(port):
             quit(cap)
 
         # listen for arduino input to start a match
-        try:
-            with serial.Serial(port, 115200, timeout=1.0) as ser:
-                if ser.in_waiting > 0:
-                    global playerLies
-                    data = ser.readline().decode('utf-8').strip()
-                    if data == "START":
-                        if current_move in MOVES:
-                            player_move = current_move
-                            computer_move = np.random.choice(MOVES)
-                            winner = decideWinner(player_move, computer_move)
-                            playerSelfReport(winner, port)
+        if (checkForStartButton(ser, frame, computer_text)== True):
+            print("Start pressed")
+            if current_move in MOVES:
+                player_move = current_move
+                computer_move = np.random.choice(MOVES)
+                winner = decideWinner(player_move, computer_move)
+                
+                computer_text = f"Computer: {computer_move}"
+                cv2.imshow("RPS with gesture_recognizer.task", frame)
+                playerSelfReport(winner, ser, computer_text, frame)
 
-                            print("\n--- ROUND ---")
-                            print("Player:   ", player_move)
-                            print("Computer: ", computer_move)
-                            print("Winner:   ", winner)
 
-                        rounds -= 1
 
-                        # simple file logging
-                        with open("rps_results.csv", "a") as f:
-                            f.write(f"{player_move},{computer_move},{winner}\n")
-                    else:
-                        print("\nNo valid move detected. Show a clear gesture and try again.")
+                print("\n--- ROUND ---")
+                print("Player:   ", player_move)
+                print("Computer: ", computer_move)
+                print("Winner:   ", winner)
 
-        except serial.SerialException as e:
-            print(f"Could not open port {port}: {e}")
-            sys.exit(1)
+                rounds -= 1
 
+            else:
+                print("\nNo valid move detected. Show a clear gesture and try again.")
+     
     gameEnd(cap)
     
 
@@ -187,43 +185,59 @@ def portSetup():
 
 # determnines the outcome of a match
 def decideWinner(player, computer):
-    print("decideWinner")
     if player == computer:
+        print("draw")
         return "draw"
     if (player == "rock" and computer == "scissors") or \
        (player == "paper" and computer == "rock") or \
        (player == "scissors" and computer == "paper"):
+        print("win")
         return "player"
+    print("loss")
     return "computer"
 
-# check what the player self-reported and determine if its a lie
-def playerSelfReport(result, port):
-    reported = False
-    try:
-        with serial.Serial(port, 115200, timeout=1.0) as ser:
-            while reported == False:
-                if ser.in_waiting > 0:
-                    global playerLies
-                    data = ser.readline().decode('utf-8').strip()
-                    if data == "WIN":
-                        print("You have reported a victory")
-                        if result != "player":
-                            playerLies += 1
-                        reported = True
-                    elif data == "LOSS":
-                        print("You have reported a loss")
-                        if result != "computer":
-                            playerLies += 1
-                        reported = True
-                    elif data == "DRAW":
-                        print("You have reported a draw")
-                        if result != "draw":
-                            playerLies += 1
-                        reported = True
+# check if the player has pressed the start button
+def checkForStartButton(ser, frame, computer_text):
+    start = False
+    if ser.in_waiting > 0:
+        data = ser.readline().decode('utf-8').strip()
+        if data == "START":
+            start = True
+            if computer_text != "":
+                cv2.putText(frame, computer_text, (145, 200),
+                            cv2.FONT_HERSHEY_SIMPLEX, 1.2, (255, 255, 255), 3)
+                
+                cv2.putText(frame,
+                            "Input if you won, lost or the match ended in a draw",
+                            (25,240),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 2)          
 
-    except serial.SerialException as e:
-        print(f"Could not open port {port}: {e}")
-        sys.exit(1)
+    return start
+
+
+# check what the player self-reported and determine if its a lie
+def playerSelfReport(result, ser, computer_text, frame):
+    reported = False
+    while reported == False:
+        if ser.in_waiting > 0:
+            global playerLies
+            data = ser.readline().decode('utf-8').strip()
+            if data == "WIN":
+                print("You have reported a victory")
+                if result != "player":
+                    playerLies += 1
+                reported = True
+            elif data == "LOSS":
+                print("You have reported a loss")
+                if result != "computer":
+                    playerLies += 1
+                reported = True
+            elif data == "DRAW":
+                print("You have reported a draw")
+                if result != "draw":
+                    playerLies += 1
+                reported = True
+
 
 # game end after set number of rounds
 def gameEnd(cap):
@@ -233,8 +247,9 @@ def gameEnd(cap):
 
 # determine & display which percentage of the player reports where false
 def playerReliability():
-    trust = 100 - (playerLies*100/3)
-    lieDetection = "You told the truth " + str(trust) + "% of the time"
+    trust = 100 - (playerLies*100/rounds)
+    round(trust, 1)
+    lieDetection = "You to ld the truth " + str(trust) + "% of the time"
     print(lieDetection)
 
 # close the game
